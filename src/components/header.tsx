80f604a49a135d3fdbe24a1dce3d5bc6f3f2b53a
Luka Tegeltija <lukategeltija@Lukas-MacBook-Air.local>
2025-04-05 09:33:14 +0200

    add mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,16 +9,16 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
-import { Brain } from "lucide-react";
+import MobileSidebar from "./mobile-sidebar";
+import Logo from "./logo";
 
 export function Header() {
   return (
     <header className="fixed w-full top-0 z-50 border-b border-border/40 backdrop-blur-sm bg-background/80">
       <div className="flex h-16 items-center px-4">
-        <div className="flex items-center space-x-2">
-          <Brain className="h-8 w-8 text-primary" />
-          <span className="text-xl font-bold">Teach.me</span>
-        </div>
+        <MobileSidebar />
+
+        <Logo />
 
         <div className="ml-auto flex items-center space-x-4">
           {/* Saved for dev purposes  */}

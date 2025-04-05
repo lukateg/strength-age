860dd0389fab33226bf2078c85f67f73f4e42c2c
Luka Tegeltija <lukategeltija@Lukas-MacBook-Air.local>
2025-04-05 11:54:04 +0200

    deploy fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,9 +16,9 @@ export function Header() {
   return (
     <header className="fixed w-full top-0 z-50 border-b border-border/40 backdrop-blur-sm bg-background/80">
       <div className="flex h-16 items-center px-4">
-        {/* <MobileSidebar /> */}
+        <MobileSidebar />
 
-        {/* <Logo /> */}
+        <Logo />
 
         <div className="ml-auto flex items-center space-x-4">
           {/* Saved for dev purposes  */}

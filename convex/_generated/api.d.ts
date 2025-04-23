221e3c2d8c08cebe01cbbfdd77d4b7ee0887531a
Luka Tegeltija <lukategeltija@Lukas-MacBook-Air.local>
2025-04-23 10:47:46 +0200

    implement webhooks for storing users in database

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,9 +14,11 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as classes from "../classes.js";
+import type * as http from "../http.js";
 import type * as lessons from "../lessons.js";
 import type * as materials from "../materials.js";
 import type * as tests from "../tests.js";
+import type * as users from "../users.js";
 import type * as utils_utils from "../utils/utils.js";
 
 /**
@@ -29,9 +31,11 @@ import type * as utils_utils from "../utils/utils.js";
  */
 declare const fullApi: ApiFromModules<{
   classes: typeof classes;
+  http: typeof http;
   lessons: typeof lessons;
   materials: typeof materials;
   tests: typeof tests;
+  users: typeof users;
   "utils/utils": typeof utils_utils;
 }>;
 export declare const api: FilterApi<

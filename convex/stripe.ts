bec062c837df54a4d98113492e5d520fd2e5c1cd
Luka Tegeltija <lukategeltija@Lukas-MacBook-Air.local>
2025-06-06 15:20:37 +0200

    install stripe

--- convex/stripe.ts
+++ convex/stripe.ts
@@ -17,7 +17,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-05-28.basil",
 });
 
 export const handleStripeCheckout = action({

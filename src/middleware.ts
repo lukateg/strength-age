0be6c929881e8359357e1ec778393746bf079936
Luka Tegeltija <lukategeltija@Lukas-MacBook-Air.local>
2025-09-01 12:27:51 +0200

    remove middleware code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,38 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-const protectedRoute = createRouteMatcher(["/app(.*)"]);
-const publicRoute = createRouteMatcher(["/"]);
+// const protectedRoute = createRouteMatcher(["/app(.*)"]);
+// const publicRoute = createRouteMatcher(["/"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth();
-  if (userId && publicRoute(req)) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/app";
+// export default clerkMiddleware(async (auth, req) => {
+//   const { userId, redirectToSignIn } = await auth();
+//   if (userId && publicRoute(req)) {
+//     const url = req.nextUrl.clone();
+//     url.pathname = "/app";
 
-    return NextResponse.redirect(url);
-  }
+//     return NextResponse.redirect(url);
+//   }
 
-  if (!userId && protectedRoute(req)) {
-    return redirectToSignIn();
-  }
+//   if (!userId && protectedRoute(req)) {
+//     return redirectToSignIn();
+//   }
 
-  // Add X-Robots-Tag header for protected routes to prevent indexing
-  if (protectedRoute(req)) {
-    const response = NextResponse.next();
-    response.headers.set(
-      "X-Robots-Tag",
-      "noindex, nofollow, noarchive, nosnippet"
-    );
-    return response;
-  }
-});
+//   // Add X-Robots-Tag header for protected routes to prevent indexing
+//   if (protectedRoute(req)) {
+//     const response = NextResponse.next();
+//     response.headers.set(
+//       "X-Robots-Tag",
+//       "noindex, nofollow, noarchive, nosnippet"
+//     );
+//     return response;
+//   }
+// });
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
